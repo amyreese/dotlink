432e444fc5de94fb21184653a1d874ef6650a79d
John Reese <john@noswap.com>
2013-09-09 18:07:41 -0700

    Feature bump

--- dotlink/dotlink.py
+++ dotlink/dotlink.py
@@ -14,7 +14,7 @@ import tempfile
 
 from os import path
 
-VERSION = '0.3.1'
+VERSION = '0.4.0'
 
 class Dotlink(object):
     """Copy or symlink dotfiles from a profile repository to a new location,
